c299576b59ce0838870540c67e1ba7de74b86dbc
maisarahishak <maisarah2299@gmail.com>
2022-04-02 20:46:12 +0800

    rework on textbox and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,29 @@
-// ignore_for_file: deprecated_member_use, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    const appTitle = 'MAD Assignment 1';
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text(appTitle),
-          ),
-          body: const MyCustomForm()),
-    );
+  void buttonPressed() {
+    print('Button Pressed');
   }
-}
-
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({Key? key}) : super(key: key);
 
   @override
+  var fruit = ['Apple', 'Orange', 'Rambutan', 'Mangosteen'];
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: const <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter a number from 1-4',
-            ),
-          ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MAD Assignment 1'),
         ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: RaisedButton(child: Text('Submit'), onPressed: null),
-        ),
-      ],
+        body: Column(children: [
+          const TextField(
+              decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Enter a number from 1-4',
+          )),
+          RaisedButton(child: Text('Answer 1'), onPressed: buttonPressed)
+        ]),
+      ),
     );
   }
 }
